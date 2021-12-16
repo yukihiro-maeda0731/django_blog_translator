bf0c209f6b76d78f1158ee150d819cb64ad29577
前田 <sloth19920731@gmail.com>
2021-12-16 23:23:12 +0900

    294まで：翻訳用にURL作成

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('translate/', include('translator.urls'))
 ]
---
+++ translator/urls.py
@@ -0,0 +1,7 @@
+from django.urls.resolvers import URLPattern
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.translator_view, name='translator_view')
+]
