71339d605db8df8328522c0910ab1589e72a3d3f
前田 <sloth19920731@gmail.com>
2021-12-18 23:47:39 +0900

    296最後まで：翻訳前に、大文字に変換するテキストエリアで動作確認

--- translator/views.py
+++ translator/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def translator_view(request):
-    return render(request, 'translator.html')
+    if request.method == 'POST':
+        original_text = request.POST['my_textarea']
+        output = original_text.upper()
+        return render(request, 'translator.html', {'output_text': output, 'original_text': original_text})
+    else:
+        return render(request, 'translator.html')
